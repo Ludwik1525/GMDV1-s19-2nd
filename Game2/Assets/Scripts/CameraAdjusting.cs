ceaef087ed318481d9aca910b8c7822d408aa00e
Ludwik1526 <mmichal1526@gmail.com>
2019-02-23 17:13:46 +0100

    Camera controls, new player texture

---
+++ Game2/Assets/Scripts/CameraAdjusting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdjusting : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float rotationSpeed = 5;
+    public float rotationDegrees = 90;
+    [SerializeField]
+    private float eulerAngX;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate () {
+
+        eulerAngX = transform.localEulerAngles.x;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (eulerAngX > 20)
+            {
+                rb.transform.Rotate(-(rotationDegrees) * Time.deltaTime * rotationSpeed, 0f, 0f);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if(eulerAngX<38)
+            {
+                rb.transform.Rotate((rotationDegrees) * Time.deltaTime * rotationSpeed, 0f, 0f);
+            }
+        }
+        
+    }
+}
