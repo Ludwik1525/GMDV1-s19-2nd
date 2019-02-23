3afa9d8221d38d9d65c511017a304c21235ec829
Ludwik1526 <mmichal1526@gmail.com>
2019-02-23 18:03:32 +0100

    Intro scene

---
+++ Game2/Assets/Scripts/StartGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+
+    public Rigidbody rb;
+    private bool pressedOnce = false;
+    public Animator anim;
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	void Update () {
+
+        if (Input.anyKeyDown && !pressedOnce)
+        {
+            anim.Play("Slide");
+            Input.ResetInputAxes();
+            pressedOnce = true;
+        }
+
+        if (Input.anyKeyDown && pressedOnce)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+	}
+}
