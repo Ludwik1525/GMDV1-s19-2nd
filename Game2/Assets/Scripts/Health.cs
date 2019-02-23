c85bfe49a017efded0a7bca23b242eb7c171e5a4
Ludwik1526 <mmichal1526@gmail.com>
2019-02-23 22:25:50 +0100

    Health and thirst bars

---
+++ Game2/Assets/Scripts/Health.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    public UnityEngine.UI.Image healthBarEmpty;
+    public float fullHealth;
+    public float emptyHealth;
+    //variable to set DMG dealt by obstacles
+    public float obstaclesDMG;
+    //variable to set DMG dealt by enemies
+    public float enemiesDMG;
+    //variable to determine whether the player entered an obstacle
+    public bool enterObstacle;
+    //variable to determine whether the player entered an enemy
+    public bool enterEnemy;
+    //variable to determine whether the player entered food
+    public bool enterFood;
+    //variable to store amount of health restored by food
+    public float HPrestore;
+    //variable to store current x coordinate of health bar
+    public float currentX;
+    
+	void Start ()
+    {
+        //assign health bar x coordinate to max health at first
+        currentX = healthBarEmpty.GetComponent<RectTransform>().anchoredPosition.x;
+    }
+	
+	void Update () {
+
+        if(enterObstacle)
+        {
+            //deal damage if obstacle entered
+            if (currentX > emptyHealth)
+            {
+                healthBarEmpty.transform.position -= new Vector3(1f * Time.deltaTime * obstaclesDMG, 0, 0);
+                currentX -= 1f * Time.deltaTime * obstaclesDMG;
+            }
+
+            //load defeat scene if health below 0
+            if (currentX <= emptyHealth)
+            {
+                SceneManager.LoadScene("Defeat");
+            }
+        }
+
+        if (enterEnemy)
+        {
+            //deal damage if enemy entered
+            if (currentX > emptyHealth)
+            {
+                healthBarEmpty.transform.position -= new Vector3(1f * Time.deltaTime * enemiesDMG, 0, 0);
+                currentX -= 1f * Time.deltaTime * enemiesDMG;
+            }
+
+            //load defeat scene if health below 0
+            if (currentX < emptyHealth)
+            {
+                SceneManager.LoadScene("Defeat");
+            }
+        }
+
+        if (enterFood)
+        {
+            //restore HP 
+            if (currentX + HPrestore > fullHealth)
+            {
+                healthBarEmpty.transform.position += new Vector3(fullHealth-currentX, 0, 0);
+                currentX = fullHealth;
+                enterFood = false;
+            }
+            else
+            {
+                healthBarEmpty.transform.position += new Vector3(HPrestore, 0, 0);
+                currentX += HPrestore;
+                enterFood = false;
+            }
+        }
+    }
+
+    //function to determine whether the player entered any obstacle/enemy/food
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Obstacle")
+        {
+            enterObstacle = true;
+        }
+
+        if (col.gameObject.tag == "Enemy")
+        {
+            enterEnemy = true;
+        }
+
+        if (col.gameObject.tag == "Food")
+        {
+            if(currentX<fullHealth)
+            {
+                enterFood = true;
+                col.gameObject.SetActive(false);
+            }
+        }
+    }
+    
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Obstacle")
+        {
+            enterObstacle = false;
+        }
+
+        if (col.gameObject.tag == "Enemy")
+        {
+            enterEnemy = false;
+        }
+    }
+
+}
---
+++ Game2/Assets/Scripts/Thirst.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Thirst : MonoBehaviour
+{
+
+    public UnityEngine.UI.Image thirstBarEmpty;
+    public UnityEngine.UI.Image healthBarEmpty;
+    public float fullThirst;
+    public float emptyThirst;
+    //variable to store rate of dehydration while not running
+    public float dehydrationRateNormal;
+    //variable to store rate of dehydration while running
+    public float dehydrationRateRunning;
+    //variable to determine whether the player entered drink
+    public bool enterDrink;
+    //variable to store amount of health restored by drink
+    public float thirstRestore;
+    //variable to store current x coordinate of thirst bar
+    public float currentThirstX;
+    public float emptyHealth;
+    //variable to store current x coordinate of health bar
+    public float currentHealthX;
+    //variable to set HP loss rate if thirst bar is empty
+    public float HPlossRate;
+
+    void Start()
+    {
+        //assign health bar and thirst bar x coordinates to max health at first
+        currentThirstX = thirstBarEmpty.GetComponent<RectTransform>().anchoredPosition.x;
+        currentHealthX = healthBarEmpty.GetComponent<RectTransform>().anchoredPosition.x;
+    }
+
+    void Update()
+    {
+
+        if (currentThirstX > emptyThirst)
+        {
+            //set thirst loss while running
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.Space))
+            {
+                thirstBarEmpty.transform.position -= new Vector3(1f * Time.deltaTime * dehydrationRateRunning, 0, 0);
+                currentThirstX -= 1f * Time.deltaTime * dehydrationRateRunning;
+            }
+            //set thirst loss while not running
+            else
+            {
+                thirstBarEmpty.transform.position -= new Vector3(1f * Time.deltaTime * dehydrationRateNormal, 0, 0);
+                currentThirstX -= 1f * Time.deltaTime * dehydrationRateNormal;
+            }
+        }
+
+        //set what happens when thirst bar is empty
+        if (currentThirstX <= emptyThirst)
+        {
+            if (currentHealthX > emptyHealth)
+            {
+                healthBarEmpty.transform.position -= new Vector3(1f * Time.deltaTime * HPlossRate, 0, 0);
+                currentHealthX -= 1f * Time.deltaTime * HPlossRate;
+            }
+        }
+
+        //load defeat scene when health bar is empty
+        if (currentHealthX < emptyHealth)
+        {
+            SceneManager.LoadScene("Defeat");
+        }
+
+        //restore thirst
+        if (enterDrink)
+        {
+            if (currentThirstX + thirstRestore > fullThirst)
+            {
+                thirstBarEmpty.transform.position += new Vector3(fullThirst-currentThirstX, 0, 0);
+                currentThirstX = fullThirst;
+                enterDrink = false;
+            }
+            else
+            {
+                thirstBarEmpty.transform.position += new Vector3(thirstRestore, 0, 0);
+                currentThirstX += thirstRestore;
+                enterDrink = false;
+            }
+        }
+    }
+
+    //function to determine whether the player entered any drink
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Drink")
+        {
+            if (currentThirstX < fullThirst)
+            {
+                enterDrink = true;
+                col.gameObject.SetActive(false);
+            }
+        }
+    }
+}
