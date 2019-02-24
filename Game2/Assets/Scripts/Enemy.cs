41975763848a7170d7e897738eb464aa6a4c3a13
RuneSChr <runechr2014@gmail.com>
2019-02-24 22:35:19 +0100

    ChibiMummy
    Added chibi mummy and put it under asset store stuff

---
+++ Game2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+public Transform playerPos;
+public float hp;
+public Rigidbody rb;
+
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+		hp = 30;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        navMeshAgent.destination = playerPos.position;
+		hp--;
+		if(hp <= 0){
+			
+		}
+
+	}
+
+	
+}
