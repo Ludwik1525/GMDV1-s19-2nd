cccf77b4c54b129c7daf64c029d2d400f38dabde
Ludwik1526 <mmichal1526@gmail.com>
2019-02-28 22:59:32 +0100

    Title scene, victory scene, other scenes enhanced

---
+++ Game2/Assets/Scripts/DefeatCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefeatCounter : MonoBehaviour {
+
+    void Start()
+    {
+        StartCoroutine(Counter(40, GetComponent<Text>()));
+    }
+
+
+    public IEnumerator Counter(float t, Text i)
+    {
+        while (int.Parse(i.text) > 0)
+        {
+            i.text = "" + (int.Parse(i.text)-1);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ Game2/Assets/Scripts/TextFadeIn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TextFadeIn : MonoBehaviour
+    {
+
+    public float time;
+    public float delay = 440;
+    public string NewLevel;
+
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+        StartCoroutine(FadeTextToFullAlpha(time, GetComponent<Text>()));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(NewLevel);
+    }
+
+    public IEnumerator FadeTextToFullAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+}
