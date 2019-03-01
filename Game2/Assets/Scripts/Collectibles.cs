b73c22599e12593076387eee3ae4f9b20574ed96
Ludwik1526 <mmichal1526@gmail.com>
2019-03-01 01:35:25 +0100

    Added collectibles and tumbleweed

---
+++ Game2/Assets/Scripts/Collectibles.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Collectibles : MonoBehaviour
+{
+
+    public AudioClip collect;
+    public AudioSource source;
+    public Text current;
+    private int counter=0;
+    public int max;
+
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+        if (counter == max)
+        {
+            SceneManager.LoadScene("VictoryScene");
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag=="Collectible")
+        {
+            col.gameObject.SetActive(false);
+            source.PlayOneShot(collect);
+            counter++;
+            current.text = "" + counter;
+        }
+    }
+}
---
+++ Game2/Assets/Scripts/TumbleweedMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumbleweedMovement : MonoBehaviour
+{
+
+    public float speed;
+    private float nextActionTime = 0.0f;
+    public float period;
+    private Vector3 direction;
+
+    void Start()
+    {
+        direction = new Vector3(Random.Range(-1.0f, 1.0f), 0.0f, Random.Range(-1.0f, 1.0f));
+        gameObject.transform.eulerAngles += direction;
+    }
+
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            direction = new Vector3(Random.Range(-1.0f, 1.0f), 0.0f, Random.Range(-1.0f, 1.0f));
+                gameObject.transform.eulerAngles += direction;
+            }
+        transform.position += direction * (speed+ Random.Range(-2.0f, 6.0f)) * Time.deltaTime;
+    }
+}
+    
