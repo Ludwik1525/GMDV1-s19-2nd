5797d7dfa81e287f242f293ed87befaa019377a8
Ludwik1526 <mmichal1526@gmail.com>
2019-03-02 15:08:32 +0100

    Final scene v2

---
+++ Game2/Assets/Scripts/DarkWasTheNight.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkWasTheNight : MonoBehaviour
+{
+    public Light lt;
+    private bool isDarkening = false;
+    private bool isBrightening = true;
+    public float time=20;
+
+	void Start ()
+    {
+    }
+	
+	void Update () {
+
+        if (lt.gameObject.transform.rotation.eulerAngles.x > 175)
+        {
+            if(!isDarkening)
+            {
+                isDarkening = true;
+                isBrightening = false;
+            }
+        }
+
+        else
+        {
+            if(!isBrightening)
+            {
+                isBrightening = true;
+                isDarkening = false;
+            }
+        }
+
+        if (isDarkening)
+        {
+            if(lt.intensity>0)
+            {
+                lt.intensity -= 0.5f * (Time.deltaTime / time);
+            }
+        }
+        if(isBrightening)
+        {
+            if(lt.intensity<0.9f)
+            {
+                lt.intensity += 0.5f * (Time.deltaTime / time);
+            }
+        }
+
+    }
+}
